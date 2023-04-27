3670327dfdf6e38a915e701b2e1a05c0f2d9997d
vaalmo <valentinamoralesvillada17@outlook.com>
2023-04-27 00:28:15 -0500

    config file updated

--- server/config.js
+++ server/config.js
@@ -6,10 +6,10 @@ export const PORT = 4000;
 import { Pool } from 'postgres-pool';
 
 export const pool = new Pool({
-    host: 'localhost',
-    database: 'parkingpass',
-    user: 'postgres',
-    password: 'vaalmopostgres',
-    port: 5432,
+    host: 'xxxx',
+    database: 'xxxx',
+    user: 'xxxx',
+    password: 'xxxx',
+    port: xxxx,
   });
 
